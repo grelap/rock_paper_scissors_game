5d02a848847d8ca7cf4b002aa067b549dc018a85
xenogonx <pablez@wp.pl>
2017-03-26 21:58:37 +0200

    html_structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,16 @@
-function rysujChoinke (x) {
-  for (var i = 1; i <= x; i++){ 
-    var star = ""
-     
-      for (var n = 0; n < i*2-1; n++){ 
-         star += '*'; 
-      } 
-    console.log(star); 
-  } 
-} 
+var newGameBtn = document.getElementById('js-newGameButton');
 
- console.log ('-------dla 1---------------');
- rysujChoinke (1);
- console.log ('-------dla 2---------------');
- rysujChoinke (2);
- console.log ('-------dla 5---------------');
- rysujChoinke (5);
+newGameBtn.addEventListener('click', newGame);
 
+//co będzie się działo po kliknięciu przycisków
+var pickRock = document.getElementById('js-playerPick_rock'),
+     pickPaper = document.getElementById('js-playerPick_paper'),
+     pickScissors = document.getElementById('js-playerPick_scissors');
+
+pickRock.addEventListener('click', function() { playerPick('rock') });
+pickPaper.addEventListener('click', function() { playerPick('paper') });
+pickScissors.addEventListener('click', function() { playerPick('scissors') });
+// --------------------
+//----wartości początkowe-------
 
 
